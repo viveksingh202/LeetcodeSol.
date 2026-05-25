717daa74727d484be0f6d1e2c12e0de5141f1b31
viveksingh202 <vy573657@gmail.com>
2026-05-25 23:58:30 +0530

    Time: 0 ms (100%), Space: 29.7 MB (59.51%) - LeetHub

---
+++ 0495-teemo-attacking/0495-teemo-attacking.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int findPoisonedDuration(vector<int>& timeSeries, int duration) {
+        
+        int total = 0;
+
+        for(int i = 0; i < timeSeries.size() - 1; i++) {
+
+            total += min(duration, timeSeries[i + 1] - timeSeries[i]);
+        }
+
+        total += duration;
+
+        return total;
+    }
+};
